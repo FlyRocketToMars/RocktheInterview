57764ee18696dfdce8ef3f944082757eb9ceb838
kdliaokueida <craigkdliao@gmail.com>
2026-01-31 11:33:02 -0800

    update json answer to list

--- app/components/interview_questions.py
+++ app/components/interview_questions.py
@@ -389,6 +389,13 @@ def render_interview_questions():
             answer = q.get("answer", "")
             if answer.startswith("```"):
                 st.code(answer.replace("```python", "").replace("```", ""), language="python")
+            elif isinstance(answer, list):
+                if len(answer) > 0 and answer[0].startswith("```python") and answer[-1].endswith("```"):
+                    answer = "\n".join(answer[1:-1])
+                    st.code(answer, language="python")
+                else:
+                answer = "\n".join(answer)
+                st.markdown(answer)
             else:
                 st.markdown(answer)
             
